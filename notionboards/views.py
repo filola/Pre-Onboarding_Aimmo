9a17beb962ec252d8b52173d2f0fe8325da7fd42
fall031 <fall900802@gmail.com>
2021-11-02 22:07:23 +0900

    ADD: 댓글,대댓글 기능 추가

--- notionboards/views.py
+++ notionboards/views.py
@@ -5,7 +5,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.http import JsonResponse
 
 from users.decorator import login_decorator
-from .models import Post
+from .models import Post, Comment
 from django.views import View
 
 class ListView(View):
@@ -65,7 +65,26 @@ class PostView(View):
                 "update_at" : post.updated_at.strftime('%Y-%m-%d %H:%M:%S'),
                 "nickname" : post.user.nickname
             }
-            return JsonResponse({"Result" : result}, status=200)
+
+            if not Post.objects.filter(id=post_id).exists():
+                return JsonResponse({"MESSAGE":"POST_DOES_NOT_EXIST"}, status=404)
+            if not Comment.objects.filter(posting=post_id).exists():
+                return JsonResponse({"MESSAGE":"COMMENT_DOES_NOT_EXIST"}, status=404)
+
+            limit = int(request.GET.get("limit",3))
+            offset = int(request.GET.get("offset",0))
+
+            comments = Comment.objects.filter(posting_id=post_id, parent_comment=None)
+
+            Result_comment = [{
+            "comment_id": comment.id,
+            "content" : comment.content,
+            "parent_comment" : comment.parent_comment_id
+            } for comment in comments]
+
+            Result_comment = Result_comment[offset:offset+limit]
+
+            return JsonResponse({"Result" : result, "Comment" : Result_comment}, status=200)
         except Post.DoesNotExist:
             return JsonResponse({"message" : "POSTS_NOT_FOUND"}, status=404)
 
@@ -107,3 +126,97 @@ class PostView(View):
         
         except ObjectDoesNotExist:
             return JsonResponse({'message': 'POST_DOES_NOT_EXIST'}, status=404)
+    
+class CommentView(View):
+    @login_decorator
+    def post(self, request, post_id):
+        try:
+            data = json.loads(request.body)
+            user = request.user
+            content = data.get("content")
+            parent_comment_id = data.get("parent_comment_id")
+            posting = Post.objects.get(id=post_id)
+
+            if not Post.objects.filter(id=post_id).exists():
+                return JsonResponse({"MESSAGE": "POSTING_DOES_NOT_EXIST"}, status=404)
+            
+            if content == None:
+                return JsonResponse({"MESSAGE": "EMPTY_CONTENT"})
+
+            comment = Comment.objects.create(
+                user    = user,
+                posting = posting,
+                content = content,
+                parent_comment_id = parent_comment_id
+            )
+
+            if not Comment.objects.get(id=comment.parent_comment_id).parent_comment_id == None:
+                return JsonResponse({"MESSAGE": "NO_MORE_COMMENT"})
+            
+            return JsonResponse({"MESSAGE": "CREATE"}, status=201)
+        except KeyError:
+            return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=400)
+
+
+    def get(self, request, post_id):
+        
+        limit = int(request.GET.get("limit",3))
+        offset = int(request.GET.get("offset",0))
+        comment_id = request.GET.get("comment_id")
+        comments = Comment.objects.filter(parent_comment=comment_id)
+
+        if not Post.objects.filter(id=post_id).exists():
+            return JsonResponse({"MESSAGE":"POST_DOES_NOT_EXIST"}, status=404)
+
+        if not Comment.objects.filter(posting=post_id).exists():
+            return JsonResponse({"MESSAGE":"COMMENT_DOES_NOT_EXIST"}, status=404)
+            
+        if not Comment.objects.filter(id=comment_id).exists():
+            return JsonResponse({"MESSAGE":"COMMENT_DOES_NOT_EXIST"}, status=404)
+
+        Result = [{
+        "comment_id": comment.id,
+        "content" : comment.content,
+        "parent_comment" : comment.parent_comment_id
+        } for comment in comments]
+
+        Result = Result[offset:offset+limit]
+
+        return JsonResponse({"Result":Result}, status=200)
+    
+    @login_decorator
+    def patch(self, request, post_id):
+        try:
+            if not Post.objects.filter(id=post_id).exists():
+                return JsonResponse({"MESSAGE":"DOES_NOT_EXIST"}, status=404)
+
+            data = json.loads(request.body)
+            content = data['content']
+            comment_id = data['comment_id']
+
+            if not request.user.id == Comment.objects.get(posting=post_id, id=comment_id).user_id:
+                return JsonResponse({"MESSAGE":"NOT_MATCHED_USER"}, status=400)
+
+            comment = Comment.objects.filter(posting_id=post_id, user=request.user.id, id=comment_id)
+            comment.update(content=content)
+            
+            return JsonResponse({"MESSAGE":"SUCCESS"}, status=201)
+
+        except KeyError:
+            return JsonResponse({"MESSAGE":"KEY_ERROR"}, status=400)
+
+    @login_decorator
+    def delete(self, request, post_id):
+        comment_id = request.GET.get("comment_id")
+
+        if not Post.objects.filter(id=post_id).exists():
+            return JsonResponse({"MESSAGE":"POST_DOES_NOT_EXIST"}, status=404)
+        if not Comment.objects.filter(id=comment_id, posting_id=post_id).exists():
+            return JsonResponse({"MASSAGE": "COMMENT_DOES_NOT_EXIST"}, status=404)
+        if not request.user.id == Comment.objects.get(posting=post_id, id=comment_id).user_id:
+                return JsonResponse({"MESSAGE":"NOT_MATCHED_USER"}, status=403)
+
+        comment = Comment.objects.filter(posting_id=post_id, user=request.user.id, id=comment_id)
+        comment.delete()
+        
+        return JsonResponse({"MESSAGE":"DELETE"}, status=204)
